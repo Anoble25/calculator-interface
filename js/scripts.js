3f8f60998e264bfa51d9e6f7b47667533521f4d4
Alex Noble and Brian Henry <brian@>
2018-03-21 10:37:34 -0700

    Add adjustments to code, it will fix all the bugs in the Javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,25 @@
-$(document).ready(function() {
+var value1;
+var value2;
 
-  $("#formOne").submit(function(event) {
-    /*var value1 = parseInt($("input#value1").val());
-    return value1 + value2;
-    var value2 = parseInt($("input#value2").val());
-    var newValue = value1+value2;
-    alert("submit works");
-    console.log("submit works");*/
-    var value1 = parseInt($("input#value1").val());
-    var value2 = parseInt($("input#value2").val());
-    event.preventDefault();
-    $("button#add").click(function(){
-      alert(value1+value2);
-    });
-    $("button#subtract").click(function(){
-      alert(value1-value2);
-    });
-    $("button#multiply").click(function(){
-      alert(value1*value2);
-    });
-    $("button#divide").click(function(){
-      alert(value1/value2);
-    });
+$(document).ready(function() {
+  getValues = function() {
+    value1 = parseInt($("input#value1").val());
+    value2 = parseInt($("input#value2").val());
+  }
+  $("button").click(function() {
+    getValues();
+  })
+
+  $("button#add").click(function(){
+    alert(value1+value2);
+  });
+  $("button#subtract").click(function(){
+    alert(value1-value2);
+  });
+  $("button#multiply").click(function(){
+    alert(value1*value2);
+  });
+  $("button#divide").click(function(){
+    alert(value1/value2);
   });
 });
